ddd77f822f98dc58fa2e242836605b6d0436c58c
Oscar Fraxedas <fraxedas@gmail.com>
2016-07-25 16:25:50 -0400

    Start with the profile controller

--- public/app.js
+++ public/app.js
@@ -28,34 +28,6 @@ app.controller('MainController', [
   },
 ]);
 
-app.controller('ProfileController', [
-  '$scope',
-  '$http',
-  'medella',
-  function ($scope, $http, medella) {
-    $scope.medella = medella;
-
-    $scope.isCalling = function () {
-      return $scope.medella.isCalling;
-    };
-
-    $scope.sos = function () {
-      return $http.post('/ping/fraxedas@gmail.com').success(function (data) {
-        $scope.medella.tittle = data;
-        $scope.medella.isCalling = true;
-      });
-    };
-
-    $scope.cancel = function () {
-      return $http.post('/ping/fraxedas@gmail.com').success(function (data) {
-        $scope.medella.tittle = data;
-        $scope.medella.isCalling = false;
-      });
-    };
-    
-  },
-]);
-
 app.factory('medella', function(){
   var medella = {
     tittle: "Medella",
