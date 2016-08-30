a102c4ccda47c24463d850b56eea94bc2eb85fed
Oscar Fraxedas <oscar.fraxedas.tormo@oracle.com>
2016-08-30 07:48:23 -0400

    UI tests are passing

--- public/test/userController.js
+++ public/test/userController.js
@@ -6,22 +6,20 @@ describe('UserController', function () {
     beforeEach(module("medella"));
 
     beforeEach(inject(function ($controller, $rootScope) {
-        /*
+
         $controllerConstructor = $controller;
         scope = $rootScope.$new();
-        medella = sinon.stub({ username: 'fraxedas@gmail.com' });
-        userData = sinon.stub({ 
+        medella = { username: 'fraxedas@gmail.com' };
+        userData = {
             get: function (user, callback) { }
-        });
-        */
+        };
+
     }));
 
     it('Set the scope', function () {
-        /*$controllerConstructor("UserController",
+        $controllerConstructor("UserController",
             { '$scope': scope, medella: medella, userData: userData });
 
         expect(scope.medella).toBe(medella);
-        */
-        expect(true).toBe(true);
     });
 });
